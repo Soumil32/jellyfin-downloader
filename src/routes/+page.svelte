3c868b96c8ee35075645691d1d50628563835e72
Soumil <soumil009@outlook.com>
2023-06-27 19:11:25 +0100

    created frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let uploadedFiles: any;
+    let typeOfContent: 'movie' | 'tv' | 'music';
+
+    const upload = async () => {
+        const formData = new FormData();
+        formData.append('files', uploadedFiles);
+        formData.append('type', typeOfContent);
+
+        const response = await fetch('/api/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await response.json();
+        console.log(data);
+    }
+
+    $: console.log(uploadedFiles);
+</script>
+
+<form>
+    <input type="file" bind:files={uploadedFiles} /> <br>
+    <select bind:value={typeOfContent}>
+        <option value="movie">Movie</option>
+        <option value="tv">TV</option>
+        <option value="music">Music</option>
+    </select>
+    <button type="submit">Upload</button>
+</form>
